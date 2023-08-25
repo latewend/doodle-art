7e55fd4e2ba29a52e31488c70b96909b4555fede
latewend <latewend@hotmail.com>
2023-08-25 17:51:55 +1000

    sdlgit add main.py

--- main.py
+++ main.py
@@ -1,1 +1,47 @@
+import ctypes
+import sdl2
+
 print("hello world")
+
+
+WIDTH = 800
+HIGHT = 600
+
+def main():
+    if sdl2.SDL_Init(sdl2.SDL_INIT_VIDEO) != 0:
+        print(sdl2.SDL_GetError())
+        return 
+    window = sdl2.SDL_CreateWindow(b"paintEm",
+                                   sdl2.SDL_WINDOWPOS_UNDEFINED,
+                                   sdl2.SDL_WINDOWPOS_UNDEFINED, WIDTH, HIGHT,
+                                   sdl2.SDL_WINDOW_OPENGL)
+    if not window:
+        print(sdl2.SDL_GetError())
+        return
+    
+    sdl2.video.SDL_GL_SetAttribute(sdl2.video.SDL_GL_CONTEXT_MAJOR_VERSION, 3)
+    sdl2.video.SDL_GL_SetAttribute(sdl2.video.SDL_GL_CONTEXT_MINOR_VERSION, 3)
+    sdl2.video.SDL_GL_SetAttribute(sdl2.video.SDL_GL_CONTEXT_PROFILE_MASK,
+        sdl2.video.SDL_GL_CONTEXT_PROFILE_CORE)
+    context = sdl2.SDL_GL_CreateContext(window)
+
+    event = sdl2.SDL_Event()
+    running = True
+
+    while running:
+        while sdl2.SDL_PollEvent(ctypes.byref(event)) != 0:
+            if event.type == sdl2.SDL_QUIT:
+                running = False
+
+        sdl2.SDL_GL_SwapWindow(window)
+        sdl2.SDL_Delay(10)
+
+    sdl2.SDL_GL_DeleteContext(context)
+    sdl2.SDL_DestroyWindow(window)
+    sdl2.SDL_Quit()
+
+if __name__ == "__main__":
+    main()
+
+
+
